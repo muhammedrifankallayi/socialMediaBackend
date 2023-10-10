b2236d5b4bb4572b6b595c9717deff3f5a56f7c5
MUHAMMED FAZIL <fazilfezz@gmail.com>
2023-10-10 08:44:46 -0700

    last

--- contoles/userControlls.js
+++ contoles/userControlls.js
@@ -3,11 +3,11 @@ const User=require('../model/userModel')
 
 const register=async(req,res)=>{
 
-
+console.log(req.body)
 const user=new User(req.body)
 user.save()
 res.status(200).send({success:true})    
-
+ 
 
 
 }
