cbbfb7719ae248f5113e79ec11704483ddf48e9a
MUHAMMED FAZIL <fazilfezz@gmail.com>
2023-10-10 20:26:51 -0700

    registration

--- contoles/userControlls.js
+++ contoles/userControlls.js
@@ -1,12 +1,28 @@
+const { query } = require('express')
 const User=require('../model/userModel')
 
 
 const register=async(req,res)=>{
+try {
 
-console.log(req.body)
-const user=new User(req.body)
-user.save()
-res.status(200).send({success:true})    
+    console.log(req.body);
+
+    const numberexis=await User.findOne({mobile:req.body.mobile})
+    const emailexist=await User.findOne({email:req.body.email})
+    if(numberexis||emailexist){
+        res.status(200).send({success:false,message:"this user aready exist"})
+    }
+   
+    else{
+        const user=new User(req.body)
+      user.save()
+      res.status(200).send({success:true,message:'user reggistration success'}) 
+
+    }
+   
+} catch (error) {
+    console.log(error.message)
+}
  
 
 
